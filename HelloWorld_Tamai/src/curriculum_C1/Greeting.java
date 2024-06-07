e7273d26902a36da2ab4b51192f2cf3c0ad08dbb
kanta <kanta@localhost>
2024-06-08 02:07:55 +0900

    課題1.20再提出します、確認お願いします。

---
+++ HelloWorld_Tamai/src/curriculum_C1/Greeting.java
@@ -0,0 +1,32 @@
+package curriculum_C1;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+
+public class Greeting {
+	 private String helloMessage;
+	    private String sushiMessage;
+	    private String sushiFact;
+	    private String currentDateTime;
+
+	    public Greeting() {
+	        this.helloMessage = "こんにちは！ここは日本です！";
+	        this.sushiMessage = "この寿司はうまい";
+	        this.sushiFact = "寿司は和食です";
+	        this.currentDateTime = getCurrentDateTime();
+	    }
+
+	    private String getCurrentDateTime() {
+	        LocalDateTime now = LocalDateTime.now();
+	        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+	        return now.format(formatter);
+	    }
+
+	    public void printGreetings() {
+	        System.out.println(this.helloMessage);
+	        System.out.println(this.sushiMessage);
+	        System.out.println(this.sushiFact);
+	        System.out.println("今の現在日時は" + this.currentDateTime + "です");
+	    }
+}
---
+++ HelloWorld_Tamai/src/curriculum_C2/Main.java
@@ -0,0 +1,10 @@
+package curriculum_C2;
+
+import curriculum_C.Greeting;
+
+public class Main {
+	public static void main(String[] args) {
+        Greeting greeting = new Greeting();
+        greeting.printGreetings();
+    }
+}
