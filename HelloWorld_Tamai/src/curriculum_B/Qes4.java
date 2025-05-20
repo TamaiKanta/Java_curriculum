f59eb273402110b4f42cf897b05680426ac8e6c8
kanta <kanta@192.168.3.14>
2025-05-20 19:42:03 +0900

    B問題_Qes4修正版

--- HelloWorld_Tamai/src/curriculum_B/Qes4.java
+++ HelloWorld_Tamai/src/curriculum_B/Qes4.java
@@ -7,7 +7,7 @@ public class Qes4{
            
             for (int multiplier = 1; multiplier <= 9; multiplier++) {
                 
-                System.out.printf("%2d * %2d = %2d", base, multiplier, (base * multiplier));
+                System.out.printf("%02d * %02d = %02d", base, multiplier, (base * multiplier));
 
                 if (multiplier < 9) {
                     System.out.print(" || ");
