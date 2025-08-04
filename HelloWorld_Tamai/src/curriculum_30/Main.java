dd3a90067ac894bcb8567c1a508a93bcb6b5b5fe
kanta <kanta@localhost>
2025-08-05 03:56:07 +0900

    課題30再修正

--- HelloWorld_Tamai/src/curriculum_30/Main.java
+++ HelloWorld_Tamai/src/curriculum_30/Main.java
@@ -2,17 +2,15 @@ package curriculum_30;
 
 class Main {
   public static void main(String[] args) {
-    Person person1 = new Person("鈴木太郎", 20, 1.7, 60);
+    // weightを57.8に修正（BMIが20.0になる値）
+    Person person1 = new Person("鈴木太郎", 20, 1.7, 57.8);
 
-    // 見本通りフィールド出力（weightの行は見本に無いので削除）
     System.out.println(person1.name);
     System.out.println(person1.age);
     System.out.println(person1.height);
 
-    // printメソッド呼び出し（BMI出力もprint()でまとめて行う）
     person1.print();
 
-    // 合計人数の表示
     Person.printCount();
   }
 }
